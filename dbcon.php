11d5770457c813457c4fad3e862f8881e45a4feb
Ric Ian Jamora <48129671+ian1447@users.noreply.github.com>
2023-05-22 10:19:14 +0800

    dbcon

--- dbcon.php
+++ dbcon.php
@@ -3,7 +3,6 @@ $host = "localhost:3306";
 //$dbname="mms";
 $dbname = "mms2";
 $username = "root";
-$password = "";
+$password = "password";
 
 $conn = mysqli_connect($host, $username, $password, $dbname);
-?>
